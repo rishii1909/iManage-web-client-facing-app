fb5623cfe347db3f218688ae5808477868db20ce
Hrushikesh Chapke <hrushikeshchapke@gmail.com>
2022-01-17 12:46:41 +0530

    Added trigger conditions to url monitor

--- components/monitors/settings/url_monitor.js
+++ components/monitors/settings/url_monitor.js
@@ -1,9 +1,12 @@
-import {Form, Input, InputNumber, Slider} from "antd";
+import {Form, Input, InputNumber, Radio, Slider} from "antd";
 import{ useState } from 'react';
 
 const {TextArea} = Input;
-const UrlMonitorSettings = () => {
+const UrlMonitorSettings = ({form}) => {
     
+    const [warn_content, setWarn_content] = useState("");
+    const [fail_content, setFail_content] = useState("");
+
 
     return (
             <>
@@ -30,17 +33,49 @@ const UrlMonitorSettings = () => {
                 </Form.Item>
 
                 <Form.Item
-                    name='failure_content'
-                    label="Failure Content"
-                >
-                    <TextArea name="content_failure" placeholder="Fail if content not found" ></TextArea>
-                </Form.Item>
-                <Form.Item
                     name='warning_content'
                     label="Warning Content"
                 >
-                    <TextArea name="content_warning" placeholder="Warn if content not found" ></TextArea>
+                    <TextArea name="content_warning" onChange={(e)=>setWarn_content(e.target.value)} placeholder="Warn if content not found" ></TextArea>
                 </Form.Item>
+
+                {warn_content && 
+                    <Form.Item
+                        name='warn_if_found'
+                        label="Warning Trigger condition"
+                    >
+                        <Radio.Group defaultValue={true} buttonStyle="solid"  >
+                            <Radio.Button value={true} >
+                                Warn if found
+                            </Radio.Button>
+                            <Radio.Button value={false} >
+                                Warn if not found
+                            </Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+                }
+
+                <Form.Item
+                    name='failure_content'
+                    label="Failure Content"
+                >
+                    <TextArea name="content_failure" onChange={(e)=>setFail_content(e.target.value)} placeholder="Fail if content not found" ></TextArea>
+                </Form.Item>
+                {fail_content && 
+                    <Form.Item
+                        name='fail_if_found'
+                        label="Failure Trigger condition"
+                    >
+                        <Radio.Group defaultValue={true} buttonStyle="solid"  >
+                            <Radio.Button value={true} >
+                                Fail if found
+                            </Radio.Button>
+                            <Radio.Button value={false} >
+                                Fail if not found
+                            </Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+                }
                
                 
             </>
