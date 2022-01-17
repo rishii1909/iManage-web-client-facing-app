6b180be7b6fc1ad4e32ecde787935df83496717e
Hrushikesh Chapke <hrushikeshchapke@gmail.com>
2022-01-17 16:22:08 +0530

    _app.js  axios.defaults.withCredentials = true

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import axios from 'axios';
 import '../styles/globals.css'
  require("antd/dist/antd.less");
  require('dotenv').config()
 
+ axios.defaults.withCredentials = true;
+ 
  String.prototype.toSentenceCase= function() {
   return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase()
  }
