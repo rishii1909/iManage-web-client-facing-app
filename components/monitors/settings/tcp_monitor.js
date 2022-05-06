8753116ad35e4bf0348d9cd486cd34f064929e69
lazyprogrammerP <runwalparshva3@gmail.com>
2022-05-06 08:45:18 +0530

    tcp monitor made similar to app

--- components/monitors/settings/tcp_monitor.js
+++ components/monitors/settings/tcp_monitor.js
@@ -1,40 +1,41 @@
-import {Form, Input, InputNumber, Slider} from "antd";
-import{ useState } from 'react';
+import { Form, Input, InputNumber, Slider } from "antd";
+import { useState } from "react";
 
-const {TextArea} = Input;
-const TCPMonitorSettings = ({hostname}) => {
-    
+const { TextArea } = Input;
+const TCPMonitorSettings = ({ hostname }) => {
+  return (
+    <>
+      <Form.Item
+        name="hostname"
+        label="Hostname"
+        style={{
+          display: "none",
+        }}
+        initialValue={hostname}
+        rules={[{ required: true, message: "Please enter the hostname." }]}
+      >
+        <Input placeholder="Enter hostname"></Input>
+      </Form.Item>
 
-    return (
-            <>
-                <Form.Item
-                    name='hostname'
-                    label="Hostname"
-                    initialValue={hostname}
-                    rules={[{required : true, message : "Please enter the hostname."}]}
-                >
-                    <Input placeholder="Enter hostname" ></Input>
-                </Form.Item>
+      <Form.Item
+        name="port"
+        label="TCP Port"
+        initialValue="80"
+        rules={[{ required: true, message: "Please enter a port." }]}
+      >
+        <Input placeholder="Enter Port"></Input>
+      </Form.Item>
 
-                <Form.Item
-                    name='port'
-                    label="Port"
-                    initialValue="80"
-                    rules={[{required : true, message : "Please enter a port."}]}
-                >
-                    <Input placeholder="Enter port"></Input>
-                </Form.Item>   
-
-                <Form.Item
-                    name='socketTimeout'
-                    label="Socket timeout"
-                    initialValue="1000"
-                    rules={[{required : true, message : "Please enter a socket timeout."}]}
-                >
-                    <Input placeholder="Enter socket timeout"></Input>
-                </Form.Item>                
-            </>
-    )
-}
+      <Form.Item
+        name="socketTimeout"
+        label="Timeout (ms)"
+        initialValue="1000"
+        rules={[{ required: true, message: "Please enter a socket timeout." }]}
+      >
+        <Input placeholder="Enter socket timeout"></Input>
+      </Form.Item>
+    </>
+  );
+};
 
 export default TCPMonitorSettings;
